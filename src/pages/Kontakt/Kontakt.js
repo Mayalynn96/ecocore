be175a4abb926e30e0b221ba7763fb5d497072d9
Maya Lynn Cohen <mayalynncohen@gmail.com>
2025-09-05 20:38:13 -0700

    added all new questions needs a little more styling and the actual sending function

--- src/pages/Kontakt/Kontakt.js
+++ src/pages/Kontakt/Kontakt.js
@@ -11,6 +11,16 @@ function Kontakt() {
     const [telNumberInput, setTelNumberInput] = useState('');
     const [adressInput, setAdressInput] = useState('');
     const [messageInput, setMessageInput] = useState('');
+    const [liegenschaftOrt, setLiegenschaftOrt] = useState('');
+    const [gebaudetyp, setGebaudetyp] = useState('');
+    const [jahressverbrauch, setJahressverbrauch] = useState('');
+    const [spezielleVerbaucher, setSpezielleVerbraucher] = useState([]);
+    const [spezielleVerbaucherSonstiges, setSpezielleVerbraucherSonstiges] = useState('');
+    const [wichtigesZiel, setWichtigesZiel] = useState('');
+    const [speicherlosung, setSpeicherlosung] = useState('');
+    const [finanzierung, setFinanzierung] = useState('');
+    const [vorbereitung, setVorbereitung] = useState('');
+    const [backupLosung, setBackupLosung] = useState('');
 
     // input handler for each state
     const handleInputChange = (e) => {
@@ -26,6 +36,12 @@ function Kontakt() {
             setAdressInput(e.target.value)
         } else if (e.target.id === "messageInput") {
             setMessageInput(e.target.value)
+        } else if (e.target.id === "liegenschaftOrt") {
+            setLiegenschaftOrt(e.target.value)
+        }  else if (e.target.id === "jahressverbrauch") {
+            setJahressverbrauch(e.target.value)
+        } else if (e.target.id === "spezielleVerbaucherSonstiges") {
+            setSpezielleVerbraucherSonstiges(e.target.value)
         }
     }
 
@@ -36,8 +52,39 @@ function Kontakt() {
         setTelNumberInput("");
         setAdressInput("");
         setMessageInput("");
+        setLiegenschaftOrt("");
+        setGebaudetyp("");
+        setJahressverbrauch("");
+        setSpezielleVerbraucher([]);
+        setSpezielleVerbraucherSonstiges("");
+        setWichtigesZiel("");
+        setSpeicherlosung("");
+        setFinanzierung("");
+        setVorbereitung("");
+        setBackupLosung("");
+    }
 
+    const handleCheckOne = (e) => {
+        const allBoxes = document.getElementsByName(e.target.name)
 
+        for (let i = 0; i < allBoxes.length; i++){
+            allBoxes[i].checked = false
+        }
+        e.target.checked = true
+
+        if (e.target.name === "gebaudetyp"){
+            setGebaudetyp(e.target.value)
+        } else if (e.target.name === "wichtigesZiel"){
+            setWichtigesZiel(e.target.value)
+        } else if (e.target.name === "speicherlosung") {
+            setSpeicherlosung(e.target.value)
+        } else if (e.target.name === "finanzierung") {
+            setFinanzierung(e.target.value)
+        } else if (e.target.name === "backupLosung") {
+            setBackupLosung(e.target.value)
+        }
+
+        
     }
 
     return (
@@ -47,8 +94,9 @@ function Kontakt() {
                 <h1>Kontakt</h1>
                 <section className='pageSection'>
                     <div id='kontaktForm'>
-                        <h2>Kontakformular</h2>
+                        <h2>Formular</h2>
                         <form>
+                            <h3>Kontakt- und Objektdaten</h3>
                             <label>Name:</label>
                             <input value={nameInput} onChange={handleInputChange} id='nameInput'></input>
                             <label>Vorname:</label>
@@ -59,8 +107,81 @@ function Kontakt() {
                             <input value={telNumberInput} onChange={handleInputChange} id='telNumberInput'></input>
                             <label>Adresse:</label>
                             <input value={adressInput} onChange={handleInputChange} id='adressInput'></input>
-                            <label>Nachricht:</label>
-                            <textarea value={messageInput} onChange={handleInputChange} id='messageInput'></textarea>
+                            <label>Liegenschaft befindet sich in:</label>
+                            <input value={liegenschaftOrt} onChange={handleInputChange} id='liegenschaftOrt'></input>
+                            <h3>Gebäudetyp</h3>
+                            <label>Einfamilienhaus (EFH)
+                            <input type="checkbox" name="gebaudetyp" value="Einfamilienhaus (EFH)" onChange={handleCheckOne}></input>
+                            </label>
+                            <label>Mehrfamilienhaus (MFH)
+                            <input type="checkbox" name="gebaudetyp" value="Mehrfamilienhaus (MFH)" onChange={handleCheckOne}></input>
+                            </label>
+                            <label>Überbauung / Zusammenschluss (ZEV / VZEV)
+                            <input type="checkbox" name="gebaudetyp" value="Überbauung / Zusammenschluss (ZEV / VZEV)" onChange={handleCheckOne}></input>
+                            </label>
+                            <h3>Aktueller Stromverbrauch</h3>
+                            <label>Jahresstromverbrauch (kWh)</label>
+                            <input type="number" value={jahressverbrauch} onChange={handleInputChange} id='jahressverbrauch'></input>
+                            <label><b>Spezielle Verbraucher (Mehrfachauswahl Checkboxen):</b></label>
+                            <label>Wärmepumpe
+                            <input type="checkbox" name="spezielleVerbaucher" value="Wärmepumpe"></input>
+                            </label>
+                            <label>Boiler / Warmwasser
+                            <input type="checkbox" name="spezielleVerbaucher" value="Boiler / Warmwasser"></input>
+                            </label>
+                            <label>Elektroauto
+                            <input type="checkbox" name="spezielleVerbaucher" value="Elektroauto"></input>
+                            </label>
+                            <label>Klimaanlage
+                            <input type="checkbox" name="spezielleVerbaucher" value="Klimaanlage"></input>
+                            </label>
+                            <label>Sonstige
+                            <input onChange={handleInputChange} name="spezielleVerbaucher" id="spezielleVerbaucherSonstiges" value={spezielleVerbaucherSonstiges} ></input>
+                            </label>
+                            <h3>Ziele & Prioritäten</h3>
+                            <label><b>Wichtigstes Ziel:</b></label>
+                            <label>Stromkosten sparen
+                            <input type="checkbox" name="wichtigesZiel" value="Stromkosten sparen" onChange={handleCheckOne}></input>
+                            </label>
+                            <label>Unabhängigkeit steigern
+                            <input type="checkbox" name="wichtigesZiel" value="Unabhängigkeit steigern" onChange={handleCheckOne}></input>
+                            </label>
+                            <label>Nachhaltigkeit / Ökologie
+                            <input type="checkbox" name="wichtigesZiel" value="Nachhaltigkeit / Ökologie" onChange={handleCheckOne}></input>
+                            </label>
+                            <label><b>Speicherlösung :</b></label>
+                            <label>Speicher von Anfang an
+                            <input type="checkbox" name="speicherlosung" value="Speicher von Anfang an" onChange={handleCheckOne}></input>
+                            </label>
+                            <label>Erst ohne Speicher starten
+                            <input type="checkbox" name="speicherlosung" value="Erst ohne Speicher starten" onChange={handleCheckOne}></input>
+                            </label>
+                            <h3>Finanzielle Rahmenbedingungen</h3>
+                            <label><b>Finanzierung :</b></label>
+                            <label>Kauf
+                            <input type="checkbox" name="finanzierung" value="Kauf" onChange={handleCheckOne}></input>
+                            </label>
+                            <label>Finanzierung / Leasing prüfen
+                            <input type="checkbox" name="finanzierung" value="Finanzierung / Leasing prüfen" onChange={handleCheckOne}></input>
+                            </label>
+                            <h3>Erweiterungen & Zukunft</h3>
+                            <label><b>Vorbereitung auf :</b></label>
+                            <label>E-Mobilität (Wallbox)
+                            <input type="checkbox" name="vorbereitung" value="E-Mobilität (Wallbox)"></input>
+                            </label>
+                            <label>Zukünftige Erweiterung PV
+                            <input type="checkbox" name="vorbereitung" value="Zukünftige Erweiterung PV"></input>
+                            </label>
+                            <label><b>Backup-Lösung bei Stromausfall :</b></label>
+                            <label>Ja
+                            <input type="checkbox" name="backupLosung" value="Ja" onChange={handleCheckOne}></input>
+                            </label>
+                            <label>Nein
+                            <input type="checkbox" name="backupLosung" value="Nein" onChange={handleCheckOne}></input>
+                            </label>
+                            <label>Vielleicht
+                            <input type="checkbox" name="backupLosung" value="Vielleicht" onChange={handleCheckOne}></input>
+                            </label>
                         </form>
                         <button onClick={handleFormSubmit}>Senden (NOT DONE)</button>
                     </div>
@@ -71,7 +192,7 @@ function Kontakt() {
                 </section>
             </main>
             <Partner />
-            <Footer/>
+            <Footer />
         </section>
     )
 }
