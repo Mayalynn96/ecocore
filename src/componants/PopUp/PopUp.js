7cb02c4dc522c1cd159cb13c3e5384ae8cf65359
Maya Lynn Cohen <mayalynncohen@gmail.com>
2025-10-31 19:10:13 -0700

    added links to ecocorefenter buttons

--- src/componants/PopUp/PopUp.js
+++ src/componants/PopUp/PopUp.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import './PopUp.css'
-import { useNavigate } from "react-router-dom";
 import EcocoreLogo from "./EcocoreLogo.webp"
 
 function PopUp() {
 // Adding useNavigate to navigate to homepage
-    const navigate = useNavigate();
-
-    // redirect to login function
-    const redirectTo = (destination) => {
-        navigate(`/${destination}`);
-    }
-
     const closePopUp = () => {
         const popUp = document.getElementById('PopUpBackground');
         console.log(popUp)
@@ -24,7 +16,7 @@ function PopUp() {
                 <img id='PopUpLogo' src={EcocoreLogo} alt='Ecocore Logo'></img>
                 <div id='PopUpBtns'>
                     <button onClick={closePopUp}>Photovoltaik</button>
-                    <button>Fenster</button>
+                    <button><a href='https://ecocorefenster.ch/' rel="noopener noreferrer">Fenster</a></button>
                 </div>
             </div>
         </div>
